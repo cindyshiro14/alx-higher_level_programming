8e25bde4f98cba6d706bd860c1635e2ff2713887
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-05 07:38:18 -0700

    My 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,28 +1,21 @@
-import py_compile
+import dis
 import sys
 
 def list_hidden_names(pyc_file):
-    # Compile the module without executing it
     try:
-        py_compile.compile(pyc_file, doraise=True)
-    except py_compile.PyCompileError as e:
-        print(f"Error compiling {pyc_file}: {e}")
+        with open(pyc_file, "rb") as f:
+            code = f.read()
+
+        # Disassemble the bytecode
+        instructions = dis.get_instructions(code)
+
+        # Extract and print names that do not start with '__'
+        for instr in instructions:
+            if instr.opname == "LOAD_NAME" and not instr.argval.startswith("__"):
+                print(instr.argval)
+    except Exception as e:
+        print(f"Error: {e}")
         sys.exit(1)
-
-    # Import the module
-    module_name = pyc_file[:-4]  # Remove '.pyc' extension
-    try:
-        module = __import__(module_name)
-    except ImportError as e:
-        print(f"Error importing {module_name}: {e}")
-        sys.exit(1)
-
-    # Get the names defined in the module
-    module_names = [name for name in dir(module) if not name.startswith("__")]
-
-    # Print the names in alphabetical order
-    for name in sorted(module_names):
-        print(name)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
