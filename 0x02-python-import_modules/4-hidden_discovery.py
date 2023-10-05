64ba69dfef45de71c0e27451c8ee50e43b92c736
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-05 07:35:18 -0700

    My 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,33 @@
-#!/usr/bin/python3.8
-import uncompyle6
+import py_compile
 import sys
 
+def list_hidden_names(pyc_file):
+    # Compile the module without executing it
+    try:
+        py_compile.compile(pyc_file, doraise=True)
+    except py_compile.PyCompileError as e:
+        print(f"Error compiling {pyc_file}: {e}")
+        sys.exit(1)
+
+    # Import the module
+    module_name = pyc_file[:-4]  # Remove '.pyc' extension
+    try:
+        module = __import__(module_name)
+    except ImportError as e:
+        print(f"Error importing {module_name}: {e}")
+        sys.exit(1)
+
+    # Get the names defined in the module
+    module_names = [name for name in dir(module) if not name.startswith("__")]
+
+    # Print the names in alphabetical order
+    for name in sorted(module_names):
+        print(name)
+
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "rb") as pyc_file:
-        source_code = uncompyle6.decompile(3.8, pyc_file.read())
+    if len(sys.argv) != 2:
+        print("Usage: python3 4-hidden_discovery.py <pyc_file>")
+        sys.exit(1)
 
-    # Extract variable names
-    names = [line.split(" = ")[0] for line in source_code.splitlines() if "=" in line]
-
-    # Filter and print the names
-    for name in names:
-        if not name.startswith("__"):
-            print(name)
+    pyc_file = sys.argv[1]
+    list_hidden_names(pyc_file)
