ed767545cc594fe6e228deb30862f5ee4463e07e
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-05 06:48:54 -0700

    My 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-import pyc
+import py_compile
+import os
+import sys
+import re
 
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "rb") as pyc_file:
-        code = pyc_file.read()
+    # Compile the .pyc file if it exists
+    pyc_file = "hidden_4.pyc"
+    if not os.path.isfile(pyc_file):
+        print("File {} not found.".format(pyc_file))
+        sys.exit(1)
 
-    code_object = pyc.compile(code)
-    names = [name for name in dir(code_object) if not name.startswith('__')]
+    try:
+        py_compile.compile(pyc_file)
+    except py_compile.PyCompileError:
+        print("Compilation of {} failed.".format(pyc_file))
+        sys.exit(1)
+
+    # Import the compiled module and list its attributes (names)
+    compiled_module_name = re.sub(r'\.pyc$', '', pyc_file)
+    compiled_module = __import__(compiled_module_name)
+
+    names = [name for name in dir(compiled_module) if not name.startswith('__')]
     names.sort()
 
     for name in names:
