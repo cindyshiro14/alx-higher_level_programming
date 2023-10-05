6b5cd50f61fefd1e63041d68c0699a98c5bdf192
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-05 10:14:06 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-import pyc
-
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "rb") as pyc_file:
-        code = pyc_file.read()
-
-    code_object = pyc.compile(code)
-    names = [name for name in dir(code_object) if not name.startswith('__')]
-    names.sort()
-
-    for name in names:
-        print(name)
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a; b, add(a, b)))
