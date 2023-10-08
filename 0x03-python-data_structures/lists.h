1767fd4f524a18c780f8be70c27d4249ef3b8766
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-08 05:23:39 -0700

    My 13-is_palindrome.c

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,5 +22,9 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
+
+/* Function prototypes for new functions */
+void reverse_list(listint_t **head);
+int compare_lists(listint_t *head1, listint_t *head2);
 
 #endif /* LISTS_H */
