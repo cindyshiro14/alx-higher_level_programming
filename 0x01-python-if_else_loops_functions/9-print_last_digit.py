c84db5c27b8f61eef904a5c59337eef535b57d81
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-08 07:14:53 -0700

    My 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,10 +3,10 @@
 def print_last_digit(number):
     # Calculate the absolute value of the number to handle negative numbers
     abs_number = abs(number)
-    
+
     # Get the last digit by taking the remainder when dividing by 10
     last_digit = abs_number % 10
-    
+
     # Print the last digit and return its value
     print(last_digit, end='')
     return last_digit
