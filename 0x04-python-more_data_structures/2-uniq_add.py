02f3fe888b57ba9ece608d38c6ad3eb73768539c
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-11 11:28:40 -0700

    My 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -12,9 +12,3 @@ def uniq_add(my_list=[]):
     result = sum(unique_set)
     
     return (result)
-
-# Test the function
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 1, 4, 2, 5]
-    result = uniq_add(my_list)
-    print("Result: {:d}".format(result))
