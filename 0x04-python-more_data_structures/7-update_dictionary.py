b33b25f91713cd21ea32200225361e28b8a58060
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-11 12:09:42 -0700

    7-update_disctionary.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # Update the dictionary by assigning the value to the key
+    a_dictionary[key] = value
+
+    return a_dictionary
