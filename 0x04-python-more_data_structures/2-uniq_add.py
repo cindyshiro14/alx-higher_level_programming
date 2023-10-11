ef026a77e8fadd413e705485690a61d138e601d2
cindyshiro14 <cindyshiro14@gmail.com>
2023-10-11 11:14:55 -0700

    My 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    # Use a set to store unique integers
+    unique_set = set()
+    
+    # Iterate through the list and add unique integers to the set
+    for num in my_list:
+        unique_set.add(num)
+
+    # Sum up the unique integers in the set
+    result = sum(unique_set)
+    
+    return result
+
+# Test the function
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    print("Result: {:d}".format(result))
