e3b4a046c00d531d0ffa526257f3cd4e8cbd587f
cindyshiro14 <cindyshiro14@gmail.com>
2024-01-30 12:20:07 -0800

    My 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that takes a letter, sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    payload = {'q': q}
+    url = "http://0.0.0.0:5000/search_user"
+    
+    response = requests.post(url, data=payload)
+
+    try:
+        json_data = response.json()
+
+        if json_data:
+            print(f"[{json_data['id']}] {json_data['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
