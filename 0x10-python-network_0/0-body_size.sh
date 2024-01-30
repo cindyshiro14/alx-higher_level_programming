11c869641fd8b0762f0f3d4baa3d56afab5f921e
cindyshiro14 <cindyshiro14@gmail.com>
2024-01-30 11:07:17 -0800

    My 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Check if the number of arguments is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url=$1
-
-# Send a request to the URL using curl and store the body in a temporary file
-response=$(curl -sI "$url")
-body_size=$(echo "$response" | grep -i Content-Length | awk '{print $2}')
-
-# Display the size of the body in bytes
-echo "$body_size"
+# This script takes a URL, sends a request, and displays the size of the response body in bytes.
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
